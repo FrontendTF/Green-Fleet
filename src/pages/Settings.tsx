42db8359edb8ba0bfbb202343395b53292612c36
FrontendTF <t.feyerl@gmail.com>
2026-02-07 05:21:11 +0100

    feat: create/add Setting Page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,3 +1,18 @@
+import driverPlaceholder from "../assets/profile-picture-placeholder.svg";
+
 export default function Settings() {
-  return <h1>Settings</h1>;
+  return (
+    <>
+      <h1>Settings</h1>
+      <h2>Account</h2>
+      <div className="flex flex-col gap-5 justify-center items-center pt-5">
+        <img className="w-20 h-20" src={driverPlaceholder} />
+        <p className="text-2xl">
+          Olaf
+          <br />
+          Schütter
+        </p>
+      </div>
+    </>
+  );
 }
